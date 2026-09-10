ed7d9d26cb12bf3215de58344449fc58f69d9a2c
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-09-10 15:30:05 +0200

    feat: add cart expiration notice for authenticated users in product catalog

--- lang/es/cart.php
+++ lang/es/cart.php
@@ -5,6 +5,7 @@ return [
     'back' => 'Regresar',
     'added_products' => 'Productos añadidos',
     'empty_cart' => 'No has añadido artículos a la cesta',
+    'expiration_notice' => 'Dispones de un plazo mínimo de 30 minutos por producto para completar la compra. El plazo se actualiza cada vez que modificas la cantidad de ese producto; pasado ese tiempo, se eliminará del carrito.',
     'quantity' => 'Cantidad',
     'unit_price' => 'Precio Unitario',
     'remove' => 'Eliminar',
