8bc037cd09551e0f05debf0c0848f1cd5c8cceb9
Alejandro Moya Ruiz <alexmyruiz@gmail.com>
2026-09-10 12:24:54 +0200

    feat: add description field to Category model and update related components

--- app/Livewire/Category/CategoryShow.php
+++ app/Livewire/Category/CategoryShow.php
@@ -4,12 +4,13 @@ namespace App\Livewire\Category;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
-use App\Models\category;
+use App\Models\Category;
 
 #[Title('Ver Categorias')]
 class CategoryShow extends Component
 {
     public Category $category;
+
     public function render()
     {
         return view('livewire.category.category-show');
